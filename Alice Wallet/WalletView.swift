c89115ce2dda61d3f380f7070166f5bcd2b1f944
kukgini <kukgini@gmail.com>
2022-09-15 23:48:34 +0900

    ...

--- Alice Wallet/WalletView.swift
+++ Alice Wallet/WalletView.swift
@@ -1,101 +1,29 @@
 import SwiftUI
 import SwiftyJSON
-import UIKit
-
-// TextInput 가능한 Alert 을 띄우기 위해서 참고한 자료
-// https://www.objc.io/blog/2020/04/21/swiftui-alert-with-textfield/
-
-extension UIAlertController {
-    convenience init(alert: TextAlert) {
-        self.init(title: alert.title, message: nil, preferredStyle: .alert)
-        addTextField { $0.placeholder = alert.placeholder }
-        addAction(UIAlertAction(title: alert.cancel, style: .cancel) { _ in
-            alert.action(nil)
-        })
-        let textField = self.textFields?.first
-        addAction(UIAlertAction(title: alert.accept, style: .default) { _ in
-            alert.action(textField?.text)
-        })
-    }
-}
-
-struct AlertWrapper<Content: View>: UIViewControllerRepresentable {
-    @Binding var isPresented: Bool
-    let alert: TextAlert
-    let content: Content
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<AlertWrapper>) -> UIHostingController<Content> {
-        UIHostingController(rootView: content)
-    }
-    
-    final class Coordinator {
-        var alertController: UIAlertController?
-        init(_ controller: UIAlertController? = nil) {
-            self.alertController = controller
-        }
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        return Coordinator()
-    }
-    
-    
-    func updateUIViewController(_ uiViewController: UIHostingController<Content>, context: UIViewControllerRepresentableContext<AlertWrapper>) {
-        uiViewController.rootView = content
-        if isPresented && uiViewController.presentedViewController == nil {
-            var alert = self.alert
-            alert.action = {
-                self.isPresented = false
-                self.alert.action($0)
-            }
-            context.coordinator.alertController = UIAlertController(alert: alert)
-            uiViewController.present(context.coordinator.alertController!, animated: true)
-        }
-        if !isPresented && uiViewController.presentedViewController == context.coordinator.alertController {
-            uiViewController.dismiss(animated: true)
-        }
-    }
-}
-
-public struct TextAlert {
-    public var title: String
-    public var placeholder: String = ""
-    public var accept: String = "OK"
-    public var cancel: String = "Cancel"
-    public var action: (String?) -> ()
-}
-
-extension View {
-    public func openWalletPromptUI(isPresented: Binding<Bool>, _ alert: TextAlert) -> some View {
-        AlertWrapper(isPresented: isPresented, alert:alert, content: self)
-    }
-    public func createWalletPromptUI(isPresented: Binding<Bool>, _ alert: TextAlert) -> some View {
-        AlertWrapper(isPresented: isPresented, alert:alert, content: self)
-    }
-}
 
 struct WalletView: View {
     
     @EnvironmentObject var model: VcxModel
-    @State private var showOpenWalletPromptUI = false
-    @State private var showCreateWalletPromptUI = false
-    
+
     var body: some View {
-        List {
-            Button(action: { self.model.resetWallet() }) {
-                Text("Reset Wallet")
-            }
+        VStack {
             if self.model.walletExists {
                 if self.model.walletOpened {
-                    Text("Wallet Opened")
+                    HStack {
+                        Image(systemName:"lock.open")
+                        Text("Wallet Opened")
+                    }
                 } else {
                     Button(action: { self.model.openWallet() }) {
-                        Image(systemName:"qrcode.viewfinder")
-                        Text("Open Wallet")
+                        HStack {
+                            Image(systemName:"lock")
+                            Text("Open Wallet")
+                        }
                     }
                 }
             } else {
                 Button(action: { self.model.createWallet() }) {
+                    Image(systemName:"qrcode.viewfinder")
                     Text("Create Wallet")
                 }
             }
